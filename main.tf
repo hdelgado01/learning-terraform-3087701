af7b452ba72a50b52c71a8a8ee8790255dad922b
hdelgado01 <82874021+hdelgado01@users.noreply.github.com>
2024-09-12 15:48:27 +0100

    Update main.tf
    Updated to free tier Windows Server 2022 and t2.micro type

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["Microsoft Windows Server 2022 Base"]
   }
 
   filter {
@@ -16,7 +16,7 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
