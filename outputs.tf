0816d268c552987b1ceeb2aa925bfb995a54e0cb
hdelgado01 <82874021+hdelgado01@users.noreply.github.com>
2024-09-12 18:45:47 +0100

    added ami name on the outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "instance_ami" {
   value = aws_instance.web.ami
 }
 
+output "instance_ami_name" {
+  value = aws_instance.web.instance_ami_name
+}
+
 output "instance_arn" {
   value = aws_instance.web.arn
 }
