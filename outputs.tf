e69fdbf19da518ca24ac1d1d3e80ede746ec59f3
hdelgado01 <82874021+hdelgado01@users.noreply.github.com>
2024-09-24 15:07:54 +0100

    correct ouput variable

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,9 @@ output "instance_ami" {
   value = aws_instance.web.ami
 }
 
-output "instance_ami_name" {
-  value = aws_instance.web.instance_ami_name
-}
+#output "instance_ami_name" {
+#  value = aws_instance.web.instance_ami_name
+#}
 
 output "instance_arn" {
   value = aws_instance.web.arn
